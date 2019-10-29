6f23b0a0ccb99bc687b107586967693b1cbfd50d
Raquel Nogueira <raquelnogueira@gmail.com>
2019-10-28 23:55:49 -0200

    add cors configuration

--- server.js
+++ server.js
@@ -1,9 +1,11 @@
 const express = require('express');
 const bodyParser = require('body-parser');
 const app = express();
+const cors = require('cors');
 
 app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({ extended: true }));
+app.use(cors());
 
 const fs = require('fs');
 
